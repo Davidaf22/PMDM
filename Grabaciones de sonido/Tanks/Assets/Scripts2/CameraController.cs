0b4479dfcf621f0fdb666071f95caee225cf5ab0
Davidaf22 <56679232+Davidaf22@users.noreply.github.com>
2019-12-11 10:32:04 +0100

    Update CameraController.cs

--- Grabaciones de sonido/Tanks/Assets/Scripts2/CameraController.cs
+++ Grabaciones de sonido/Tanks/Assets/Scripts2/CameraController.cs
@@ -2,18 +2,20 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-
-    public GameObject Player;
+    
+    public GameObject Player; //objeto jugador
 
     private Vector3 offset;
 
     void Start ()
     {
+        //adquiere la posicion de salida del jugador
         offset = transform.position - Player.transform.position;
     }
     
     void LateUpdate ()
     {
+        //actuzliza la posicion de la camara a medida que se mueve el jugador
         transform.position = Player.transform.position + offset;
     }
 }
