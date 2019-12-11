e22ac1ad53e580f2c69c3fff8878cea1eab9c786
Davidaf22 <56679232+Davidaf22@users.noreply.github.com>
2019-12-11 10:33:58 +0100

    Update SampleAgentScript.cs

--- Grabaciones de sonido/Tanks/Assets/Scripts2/SampleAgentScript.cs
+++ Grabaciones de sonido/Tanks/Assets/Scripts2/SampleAgentScript.cs
@@ -2,22 +2,22 @@
 using UnityEngine.AI;
 
 public class SampleAgentScript : MonoBehaviour {
-       public Transform goal;
+       public Transform goal;//meta del agente
 
-	   private NavMeshAgent agent;
+	   private NavMeshAgent agent; //objeto agente
        
 	   //public Animator animator;
 
 	   //public float proximidad;
     void Start (){
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        agent.autoBraking = false;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();//obtenemos componente navmesh
+        agent.autoBraking = false;//seteamos autobaking false
         // asiganmos el animator del player para poder cambiar la bool
         //animator = goal.GetComponent<Animator>();
     }
 	void Update(){
         // con el player/target
-        agent.SetDestination(goal.position);
+        agent.SetDestination(goal.position);//setea el destino del agente.
         // comparamos la distancia al enemigo
         // cambiamos la variable bool
         // de esta manera forzamos la transicion desde Tarnquilo a Peligro
